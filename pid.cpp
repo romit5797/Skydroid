5aea1f76643500ca8b8d57f3cdafe8ad77511a87
Romit Singh Bhau <romitenvious@icloud.com>
2020-01-24 17:33:39 +0530

    Update pid.cpp

--- pid.cpp
+++ pid.cpp
@@ -2,21 +2,46 @@
 #include <Servo.h>
 
 
-Servo right_prop;
-Servo left_prop;
+Servo right_prop1;
+Servo left_prop1;
+Servo right_prop2;
+Servo left_prop2;
+float pid_i_roll=0,pid_p_roll,pid_d_roll;
+float pid_i_yaw=0,pid_p_yaw,pid_d_yaw;
+float pid_i_pitch=0,pid_p_pitch,pid_d_pitch;
 
 /*MPU-6050 gives you 16 bits data so you have to create some 16int constants
  * to store the data for accelerations and gyro*/
+float pid_i_mem_roll, pid_roll_setpoint, gyro_roll_input, pid_output_roll, pid_last_roll_d_error;
+float pid_i_mem_pitch, pid_pitch_setpoint, gyro_pitch_input, pid_output_pitch, pid_last_pitch_d_error;
+float pid_i_mem_yaw, pid_yaw_setpoint, gyro_yaw_input, pid_output_yaw, pid_last_yaw_d_error;
+float angle_roll_acc, angle_pitch_acc, angle_pitch, angle_roll, angle_yaw; 
+ 
+ float pid_p_gain_roll = 1.3;               //Gain setting for the pitch and roll P-controller (default = 1.3).
+float pid_i_gain_roll = 0.04;              //Gain setting for the pitch and roll I-controller (default = 0.04).
+float pid_d_gain_roll = 18.0;              //Gain setting for the pitch and roll D-controller (default = 18.0).
+int pid_max_roll = 400;                    //Maximum output of the PID-controller (+/-).
+
+float pid_p_gain_pitch = pid_p_gain_roll;  //Gain setting for the pitch P-controller.
+float pid_i_gain_pitch = pid_i_gain_roll;  //Gain setting for the pitch I-controller.
+float pid_d_gain_pitch = pid_d_gain_roll;  //Gain setting for the pitch D-controller.
+int pid_max_pitch = pid_max_roll;          //Maximum output of the PID-controller (+/-).
+
+float pid_p_gain_yaw = 4.0;                //Gain setting for the pitch P-controller (default = 4.0).
+float pid_i_gain_yaw = 0.02;               //Gain setting for the pitch I-controller (default = 0.02).
+float pid_d_gain_yaw = 0.0;                //Gain setting for the pitch D-controller (default = 0.0).
+int pid_max_yaw = 400;                     //Maximum output of the PID-controller (+/-).
 
 int16_t Acc_rawX, Acc_rawY, Acc_rawZ,Gyr_rawX, Gyr_rawY, Gyr_rawZ;
  
-
+int PID1,PID2,PID3,PID4;
+int esc_1,esc_2,esc_3,esc_4;
 float Acceleration_angle[2]= {0,0};
 float Gyro_angle[2]={0,0};
 float Total_angle[2]={0,0};
-float a,b,c,d,e;
-float err[5];
-float AccErrorX=0, AccErrorY=0, GyroErrorX=0, GyroErrorY=0, GyroErrorZ=0;
+float a,b,c=1,d,e;
+float err[6];
+float AccErrorX=0, AccErrorY=0, AccErrorZ=0, GyroErrorX=0, GyroErrorY=0, GyroErrorZ=0;
 float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
 float AccX, AccY, AccZ;
 float GyroX, GyroY, GyroZ;
@@ -27,7 +52,7 @@ float elapsedTime, time, timePrev;
 int i,j=0;
 float rad_to_deg = 180/3.141592654;
 
-float PID, pwmLeft, pwmRight, error, previous_error;
+float PID, pwmLeft, pwmRight, error_r,error_p,error_y, previous_error_r,previous_error_p,previous_error_y;
 float pid_p=0;
 float pid_i=0;
 float pid_d=0;
@@ -37,7 +62,7 @@ double ki=0.005;//0.003
 double kd=2.05;//2.05
 ///////////////////////////////////////////////
 
-double throttle=1300; //initial value of throttle to the motors
+double throttle=1050; //initial value of throttle to the motors
 float desired_angle = 0; //This is the angle in which we whant the
                          //balance to stay steady
 
@@ -49,21 +74,27 @@ void setup() {
   Wire.write(0);
   Wire.endTransmission(true);
   Serial.begin(9600);
-  right_prop.attach(3); //attatch the right motor to pin 3
-  left_prop.attach(5);  //attatch the left motor to pin 5
+  right_prop1.attach(4); //attatch the right motor to pin 3
+  right_prop2.attach(5); //attatch the right motor to pin 3
+  left_prop1.attach(6);  //attatch the left motor to pin 5
+  left_prop2.attach(7);  //attatch the left motor to pin 5
 
   time = millis(); //Start counting time in milliseconds
   /*In order to start up the ESCs we have to send a min value
    * of PWM to them before connecting the battery. Otherwise
    * the ESCs won't start up or enter in the configure mode.
    * The min value is 1000us and max is 2000us, REMEMBER!*/
-  left_prop.writeMicroseconds(1000); 
-  right_prop.writeMicroseconds(1000);
+  left_prop1.writeMicroseconds(1000); 
+  left_prop2.writeMicroseconds(1000); 
+  right_prop1.writeMicroseconds(1000);
+  right_prop2.writeMicroseconds(1000);
   Wire.beginTransmission(0x68);                   //Start communication with the MPU-6050.
   Wire.write(0x1B);                                            //We want to write to the GYRO_CONFIG register (1B hex).
   Wire.write(0x08);                                            //Set the register bits as 00001000 (500dps full scale).
   Wire.endTransmission();
+  Serial.println("Starting..");
   calculate_IMU_error();  
+  Serial.println("done with imu calibration");
 }
 
 void loop() {
@@ -73,7 +104,7 @@ void loop() {
     time = millis();  // actual time read
     elapsedTime = (time - timePrev) / 1000; 
   
-  /*The tiemStep is the time that elapsed since the previous loop. 
+  /*The tiemStep is the time that elapsed sincee previous loop. 
    * This is the value that we will use in the formulas as "elapsedTime" 
    * in seconds. We work in ms so we haveto divide the value by 1000 
    to obtain seconds*/
@@ -82,12 +113,12 @@ void loop() {
    * We know that the slave adress for this IMU is 0x68 in
    * hexadecimal. For that in the RequestFrom and the 
    * begin functions we have to put this value.*/
-     a = millis();
+     ;
      Wire.beginTransmission(0x68);
      Wire.write(0x3B); //Ask for the 0x3B register- correspond to AcX
      Wire.endTransmission(false);
      Wire.requestFrom(0x68,8,true); 
-     b = millis();
+    
    /*We have asked for the 0x3B register. The IMU will send a brust of register.
     * The amount of register to read is specify in the requestFrom function.
     * In this case we request 6 registers. Each value of acceleration is made out of
@@ -96,8 +127,11 @@ void loop() {
     * register (<<) and make an or (|) operation to add the low values.*/
     
      Acc_rawX=Wire.read()<<8|Wire.read(); //each value needs two registres
+     Acc_rawX = Acc_rawX - err[0];
      Acc_rawY=Wire.read()<<8|Wire.read();
+     Acc_rawY = Acc_rawY -err[1];
      Acc_rawZ=Wire.read()<<8|Wire.read();
+     Acc_rawZ = Acc_rawZ - err[2];
      float Temp = Wire.read()<<8|Wire.read();
      Temp = (Temp/340 + 36.53);
      Serial.println("Temp:");
@@ -119,10 +153,10 @@ void loop() {
      *  will calculate the rooth square.*/
      /*---X---*/
      Acceleration_angle[0] = atan((Acc_rawY/16384.0)/sqrt(pow((Acc_rawX/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
-     Acceleration_angle[0] = Acceleration_angle[0] - err[0];
+    
      /*---Y---*/
      Acceleration_angle[1] = atan(-1*(Acc_rawX/16384.0)/sqrt(pow((Acc_rawY/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
-     Acceleration_angle[1] = Acceleration_angle[1] - err[1];
+     
  
    /*Now we read the Gyro data in the same way as the Acc data. The adress for the
     * gyro data starts at 0x43. We can see this adresses if we look at the register map
@@ -132,10 +166,16 @@ void loop() {
    Wire.beginTransmission(0x68);
    Wire.write(0x43); //Gyro data first adress
    Wire.endTransmission(false);
-   Wire.requestFrom(0x68,4,true); //Just 4 registers
+   Wire.requestFrom(0x68,6,true); //Just 4 registers
    
    Gyr_rawX=Wire.read()<<8|Wire.read(); //Once again we shif and sum
+   Gyr_rawX = Gyr_rawX - err[3];
+  
    Gyr_rawY=Wire.read()<<8|Wire.read();
+   Gyr_rawY = Gyr_rawY - err[4];
+   
+   Gyr_rawZ=Wire.read()<<8|Wire.read();
+   Gyr_rawZ = Gyr_rawZ - err[5];
   
    /*Now in order to obtain the gyro data in degrees/seconda we have to divide first
    the raw value by 131 because that's the value that the datasheet gives us*/
@@ -144,23 +184,25 @@ void loop() {
    Gyro_angle[0] = Gyr_rawX/131.0; 
    /*---Y---*/
    Gyro_angle[1] = Gyr_rawY/131.0;
+   Gyro_angle[2] = Gyr_rawZ/131.0;
 
    /*Now in order to obtain degrees we have to multiply the degree/seconds
    *value by the elapsedTime.*/
    /*Finnaly we can apply the final filter where we add the acceleration
    *part that afects the angles and ofcourse multiply by 0.98 */
    gyroAngleX =  Gyro_angle[0]*elapsedTime;
-   gyroAngleX = gyroAngleX - err[2];
    gyroAngleY =  Gyro_angle[1]*elapsedTime;
-   gyroAngleY = gyroAngleY - err[3];
+   gyroAngleZ =  Gyro_angle[2]*elapsedTime;
+  
    
    /*---X axis angle---*/
    Total_angle[0] = 0.96 *(Total_angle[0]+gyroAngleX) + 0.04*Acceleration_angle[0];
    /*---Y axis angle---*/
    Total_angle[1] = 0.96 *(Total_angle[1]+gyroAngleY) + 0.04*Acceleration_angle[1];
+   Total_angle[2] = (Total_angle[2]+gyroAngleZ) ;
    Serial.println("..");
    j++;
-   Serial.print(j);
+   Serial.print(Total_angle[0]);
    Serial.print("/");
    Serial.print(Total_angle[1]);
    
@@ -170,89 +212,133 @@ void loop() {
    
   
 /*///////////////////////////P I D///////////////////////////////////*/
-/*Remember that for the balance we will use just one axis. I've choose the x angle
-to implement the PID with. That means that the x axis of the IMU has to be paralel to
-the balance*/
-
 /*First calculate the error between the desired angle and 
 *the real measured angle*/
-error = Total_angle[1] - desired_angle;
+error_r = Total_angle[1]*1.7 - desired_angle;
+error_p = 0;//Total_angle[1]*1.7 - desired_angle;
+error_y = 0;//Total_angle[2]*1.7 - desired_angle;
     
 /*Next the proportional value of the PID is just a proportional constant
 *multiplied by the error*/
-
-pid_p = kp*error;
-
-/*The integral part should only act if we are close to the
-desired position but we want to fine tune the error. That's
-why I've made a if operation for an error between -3 and 3 degree.
-To integrate we just sum the previous integral value with the
-error multiplied by  the integral constant. This will integrate (increase)
-the value each loop till we reach the 0 point*/
-if(-3 <error <3)
-{
-  pid_i = pid_i+(ki*error);  
+pid_p_roll = pid_p_gain_roll*error_r;
+if(-3 <error_r <3){
+     pid_i_roll = pid_i_roll+(pid_i_gain_roll*error_r) ;
 }
+pid_d_roll = pid_d_gain_roll*((error_r - previous_error_r)/elapsedTime);
 
-/*The last part is the derivate. The derivate acts upon the speed of the error.
-As we know the speed is the amount of error that produced in a certain amount of
-time divided by that time. For taht we will use a variable called previous_error.
-We substract that value from the actual error and divide all by the elapsed time. 
-Finnaly we multiply the result by the derivate constant*/
+pid_output_roll = pid_p_roll + pid_i_roll + pid_d_roll;
+#/*---------------------pitch-------------------------*/
+pid_p_pitch = pid_p_gain_pitch*error_p ;
+if(-3 <error_p <3){
+     pid_i_pitch = pid_i_pitch+(pid_i_gain_pitch*error_p) ;
+}   
+pid_d_pitch = pid_d_gain_pitch*((error_p - previous_error_p)/elapsedTime);
 
-pid_d = kd*((error - previous_error)/elapsedTime);
+pid_output_pitch = pid_p_pitch + pid_i_pitch + pid_d_pitch;
+#/*---------------------yaw-------------------------*/
+pid_p_yaw = pid_p_gain_yaw*error_y;
+if(-3 <error_y <3){
+     pid_i_yaw = pid_i_yaw+(pid_i_gain_yaw*error_y)    ;
+}
+pid_d_yaw = pid_d_gain_yaw*((error_y - previous_error_y)/elapsedTime);
+
+pid_output_yaw = pid_p_yaw + pid_i_yaw + pid_d_yaw;
+          
+
 
 /*The final PID values is the sum of each of this 3 parts*/
-PID = pid_p + pid_i + pid_d;
+PID1 = pid_output_pitch + pid_output_roll + pid_output_yaw ;
+PID2 = pid_output_pitch - pid_output_roll - pid_output_yaw ;
+PID3 = pid_output_yaw - pid_output_roll - pid_output_pitch;
+PID4 = pid_output_roll - pid_output_pitch - pid_output_yaw;
+
+if(PID1 < -400){
+     PID1=-400;
+}
+if(PID1 > 400){
+     PID1=400;
+}
+
+if(PID2 < -400){
+     PID2=-400;
+}
+if(PID2 > 400){
+     PID2=400;
+}
+
+if(PID3 < -400){
+     PID3=-400;
+}
+if(PID3 > 400){
+     PID3=400;
+}
+
+if(PID4 < -400){
+     PID4=-400;
+}
+if(PID4 > 400){
+     PID4=400;
+}
 
 /*We know taht the min value of PWM signal is 1000us and the max is 2000. So that
 tells us that the PID value can/s oscilate more than -1000 and 1000 because when we
 have a value of 2000us the maximum value taht we could sybstract is 1000 and when
 we have a value of 1000us for the PWM sihnal, the maximum value that we could add is 1000
 to reach the maximum 2000us*/
-if(PID < -1000)
-{
-  PID=-1000;
+
+/*Finally we calculate the PWM width. We sum the desired throttle and the PID value*/
+esc_1 = throttle + PID4;       //Calculate the pulse for esc 1 (front-right - CCW).
+esc_2 = throttle + PID1 ;       //Calculate the pulse for esc 2 (rear-right - CW).
+esc_3 = throttle + PID2  ;     //Calculate the pulse for esc 3 (rear-left - CCW).
+esc_4 = throttle + PID3 ;    //Calculate the pulse for esc 4 (front-left - CW).
+
+if(esc_1 < 1050){
+     esc_1 = 1050;
 }
-if(PID > 1000)
-{
-  PID=1000;
+if(esc_1 > 1400){
+     esc_1 = 1400;
 }
 
-/*Finnaly we calculate the PWM width. We sum the desired throttle and the PID value*/
-pwmLeft = throttle + PID;
-pwmRight = throttle - PID;
+if(esc_2 < 1050){
+     esc_2 = 1050;
+}
+if(esc_2 > 1400){
+     esc_2 = 1400;
+}
 
-
-/*Once again we map the PWM values to be sure that we won't pass the min
-and max values. Yes, we've already maped the PID values. But for example, for 
-throttle value of 1300, if we sum the max PID value we would have 2300us and
-that will mess up the ESC.*/
-//Right
-if(pwmRight < 1000)
-{
-  pwmRight= 1000;
+if(esc_3 < 1050){
+     esc_3 = 1050;
 }
-if(pwmRight > 2000)
-{
-  pwmRight=2000;
+if(esc_3 > 1400){
+     esc_3 = 1400;
 }
-//Left
-if(pwmLeft < 1000)
-{
-  pwmLeft= 1000;
+if(esc_4 < 1050){
+     esc_4 = 1050;
 }
-if(pwmLeft > 2000)
-{
-  pwmLeft=2000;
+if(esc_4 > 1400){
+     esc_4 = 1400;
 }
 
 /*Finnaly using the servo function we create the PWM pulses with the calculated
 width for each pulse*/
-left_prop.writeMicroseconds(pwmLeft);
-right_prop.writeMicroseconds(pwmRight);
-previous_error = error; //Remember to store the previous error.
+left_prop1.writeMicroseconds(esc_4);
+left_prop2.writeMicroseconds(esc_3);
+right_prop1.writeMicroseconds(esc_1);
+right_prop2.writeMicroseconds(esc_2);
 
+previous_error_r = error_r; //Remember to store the previous error.
+previous_error_p = error_p; //Remember to store the previous error.
+previous_error_y = error_y; //Remember to store the previous error.
+
+Serial.println("motor");
+Serial.print(esc_1);
+Serial.print("/");
+Serial.print(esc_2);
+Serial.print("/");
+Serial.print(esc_3);
+Serial.print("/");
+Serial.print(esc_4);
+Serial.print("/");
 
 }
 
@@ -261,29 +347,26 @@ void calculate_IMU_error() {
   // We can call this funtion in the setup section to calculate the accelerometer and gyro data error. From here we will get the error values used in the above equations printed on the Serial Monitor.
   // Note that we should place the IMU flat in order to get the proper values, so that we then can the correct values
   // Read accelerometer values 200 times
-  while (c < 202) {
+  while (c <= 11000) {
     Wire.beginTransmission(0x68);
     Wire.write(0x3B);
     Wire.endTransmission(false);
     Wire.requestFrom(0x68, 6, true);
      
-    AccX = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
-    AccY = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
-    AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
+    AccX = (Wire.read() << 8 | Wire.read()) ;
+    AccY = (Wire.read() << 8 | Wire.read()) ;
+    AccZ = (Wire.read() << 8 | Wire.read()); 
     // Sum all readings
-    if(c>1)
+    if(c>1000)
     {
-    AccErrorX = AccErrorX + ((atan((AccY) / sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / PI));
-    AccErrorY = AccErrorY + ((atan(-1 * (AccX) / sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / PI));
+    AccErrorX = AccErrorX + AccX;
+    AccErrorY = AccErrorY + AccY;
+    AccErrorZ = AccErrorZ + AccZ;
     }
-    c++;
-  }
-  //Divide the sum by 200 to get the error value
-  AccErrorX = AccErrorX / 200;
-  AccErrorY = AccErrorY / 200;
-  c = 0;
+  
+ 
+
   // Read gyro values 200 times
-  while (c < 202) {
     Wire.beginTransmission(0x68);
     Wire.write(0x43);
     Wire.endTransmission(false);
@@ -292,28 +375,28 @@ void calculate_IMU_error() {
     GyroX = Wire.read() << 8 | Wire.read();
     GyroY = Wire.read() << 8 | Wire.read();
     GyroZ = Wire.read() << 8 | Wire.read();
-    // Sum all readings
-    timePrev = time;  // the previous time is stored before the actual time read
-    time = millis();  // actual time read
-    elapsedTime = (time - timePrev) / 1000; 
-     if(c>1)
+    // Sum all rea
+     if(c>1000)
     {
-    GyroErrorX = GyroErrorX + (GyroX / 131.0)* elapsedTime;
-    GyroErrorY = GyroErrorY + (GyroY / 131.0)* elapsedTime;
-    GyroErrorZ = GyroErrorZ + (GyroZ / 131.0)* elapsedTime;
+    GyroErrorX = GyroErrorX + GyroX;
+    GyroErrorY = GyroErrorY + GyroY;
+    GyroErrorZ = GyroErrorZ + GyroZ;
     }
     c++;
     
   }
   //Divide the sum by 200 to get the error value
-  GyroErrorX = GyroErrorX / 200;
-  GyroErrorY = GyroErrorY / 200;
-  GyroErrorZ = GyroErrorZ / 200;
+  AccErrorX = AccErrorX / 10000;
+  AccErrorY = AccErrorY / 10000;
+  GyroErrorX = GyroErrorX / 10000;
+  GyroErrorY = GyroErrorY / 10000;
+  GyroErrorZ = GyroErrorZ / 10000;
   // Print the error values on the Serial Monitor
   err[0]= AccErrorX;
   err[1] = AccErrorY;
-  err[2] = GyroErrorX;
-  err[3] = GyroErrorY;
-  err[4] =GyroErrorZ;
-}//end of loop void
+  err[2] = AccErrorZ;
+  err[3] = GyroErrorX;
+  err[4] = GyroErrorY;
+  err[5] =GyroErrorZ;
+}
  
