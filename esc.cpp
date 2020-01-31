b5eb41899579117d27ac292736232a78ce2281ca
Romit Singh Bhau <romitenvious@icloud.com>
2020-01-31 15:36:04 +0530

    Update esc.cpp

--- esc.cpp
+++ esc.cpp
@@ -1,98 +1,70 @@
 #include <Servo.h>
-// ---------------------------------------------------------------------------
-// Customize here pulse lengths as needed
-#define MIN_PULSE_LENGTH 1000 // Minimum pulse length in µs
-#define MAX_PULSE_LENGTH 2000 // Maximum pulse length in µs
-#define DEFAULT_PULSE_WIDTH 0 // default pulse width when servo is attached
-// ---------------------------------------------------------------------------
-Servo motA, motB, motC, motD;
-char data=0;
-// ---------------------------------------------------------------------------
 
-/**
- * Initialisation routine
- */
+#define MAX_SIGNAL 2000
+#define MIN_SIGNAL 1000
+#define MOTOR_PIN_1 5
+#define MOTOR_PIN_2 6
+#define MOTOR_PIN_3 10
+#define MOTOR_PIN_4 11
+int DELAY = 1000;
+
+Servo motor1,motor2,motor3,motor4;
+
 void setup() {
-    Serial.begin(9600);
-    
-    motA.attach(4, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
-    motB.attach(5, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
-    motC.attach(6, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
-    motD.attach(7, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
-    
-    displayInstructions();
+  Serial.begin(115200);
+  Serial.println(" ");
+  delay(1500);
+  Serial.println("Program begin...");
+  delay(1000);
+  Serial.println("This program will start the ESC.");
+
+  motor1.attach(MOTOR_PIN_1);
+  motor2.attach(MOTOR_PIN_2);
+  motor3.attach(MOTOR_PIN_3);
+  motor4.attach(MOTOR_PIN_4);
+
+  Serial.print("Now writing maximum output: (");Serial.print(MAX_SIGNAL);Serial.print(" us in this case)");Serial.print("\n");
+  Serial.println("Turn on power source, then wait 2 seconds and press any key.");
+  motor1.writeMicroseconds(MAX_SIGNAL);
+  motor2.writeMicroseconds(MAX_SIGNAL);
+  motor3.writeMicroseconds(MAX_SIGNAL);
+  motor4.writeMicroseconds(MAX_SIGNAL);
+
+  // Wait for input
+  while (!Serial.available());
+  Serial.read();
+
+  // Send min output
+  Serial.println("\n");
+  Serial.println("\n");
+  Serial.print("Sending minimum output: (");Serial.print(MIN_SIGNAL);Serial.print(" us in this case)");Serial.print("\n");
+  motor1.writeMicroseconds(MIN_SIGNAL);
+  motor2.writeMicroseconds(MIN_SIGNAL);
+  motor3.writeMicroseconds(MIN_SIGNAL);
+  motor4.writeMicroseconds(MIN_SIGNAL);
+  Serial.println("The ESC is calibrated");
+  Serial.println("----");
+  Serial.println("Now, type a values between 1000 and 2000 and press enter");
+  Serial.println("and the motor will start rotating.");
+  Serial.println("Send 1000 to stop the motor and 2000 for full throttle");
+
 }
 
-/**
- * Main function
- */
 void loop() {
-    if (Serial.available()) {
-        data = Serial.read();
-
-        switch (data) {
-            // 0
-            case 48 : Serial.println("Sending minimum throttle");
-                      motA.writeMicroseconds(MIN_PULSE_LENGTH);
-                      motB.writeMicroseconds(MIN_PULSE_LENGTH);
-                      motC.writeMicroseconds(MIN_PULSE_LENGTH);
-                      motD.writeMicroseconds(MIN_PULSE_LENGTH);
-            break;
-
-            // 1
-            case 49 : Serial.println("Sending maximum throttle");
-                      motA.writeMicroseconds(MAX_PULSE_LENGTH);
-                      motB.writeMicroseconds(MAX_PULSE_LENGTH);
-                      motC.writeMicroseconds(MAX_PULSE_LENGTH);
-                      motD.writeMicroseconds(MAX_PULSE_LENGTH);
-            break;
-
-            // 2
-            case 50 : Serial.print("Running test in 3");
-                      delay(1000);
-                      Serial.print(" 2");
-                      delay(1000);
-                      Serial.println(" 1...");
-                      delay(1000);
-                      test();
-            break;
-        }
-    }
-    
-
-}
-
-/**
- * Test function: send min throttle to max throttle to each ESC.
- */
-void test()
-{
-    for (int i = 1; i<500; i += 5) {
-        Serial.print("Pulse length = ");
-        Serial.println(i);
-        
-        motA.writeMicroseconds(1200);
-        motB.writeMicroseconds(i);
-        motC.writeMicroseconds(i);
-        motD.writeMicroseconds(i);
-        
-        delay(200);
-    }
-
-    Serial.println("STOP");
-    motA.writeMicroseconds(MIN_PULSE_LENGTH);
-    motB.writeMicroseconds(MIN_PULSE_LENGTH);
-    motC.writeMicroseconds(MIN_PULSE_LENGTH);
-    motD.writeMicroseconds(MIN_PULSE_LENGTH);
-}
-
-/**
- * Displays instructions to user
- */
-void displayInstructions()
-{  
-    Serial.println("READY - PLEASE SEND INSTRUCTIONS AS FOLLOWING :");
-    Serial.println("\t0 : Send min throttle");
-    Serial.println("\t1 : Send max throttle");
-    Serial.println("\t2 : Run test function\n");
+   
+  if (Serial.available() > 0)
+  {
+    int DELAY = Serial.parseInt();
+    if (DELAY > 999)
+    {
+      
+      motor1.writeMicroseconds(DELAY);
+      motor2.writeMicroseconds(DELAY);
+      motor3.writeMicroseconds(DELAY);
+      motor4.writeMicroseconds(DELAY);
+      float SPEED = (DELAY-1000)/10;
+      Serial.print("\n");
+      Serial.println("Motor speed:"); Serial.print("  "); Serial.print(SPEED); Serial.print("%"); 
+    }     
+  }
 }
